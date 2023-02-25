0844f6e68f16511e72afd3a96b41ef55b32f9adb
Anirudh Rowjee <ani.rowjee@gmail.com>
2023-02-25 18:55:41 +0530

    feat: modified response bodies for demux
    Signed-off-by: Anirudh Rowjee <ani.rowjee@gmail.com>
    Added example responses for various endpoints

--- demux/main.go
+++ demux/main.go
@@ -14,23 +14,24 @@ func main() {
 	})
 
 	server.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		w.Write([]byte("login"))
 	})
 
 	server.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		w.Write([]byte("register"))
 	})
 
 	server.HandleFunc("/leaderboard", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		w.Write([]byte("leaderboard"))
 	})
 
 	server.HandleFunc("/challenges", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		w.Write([]byte("challenges"))
 	})
 
 	server.HandleFunc("/compare", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		// TODO Talk to the Comparator Service
+		w.Write([]byte("compare"))
 	})
 
 	http.ListenAndServe(":8080", server)
