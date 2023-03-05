ba8eb949fad9c92ebbafbf700065a2bfc6919f62
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-03-05 16:54:45 +0530

    Added: schema stuff

--- demux/database/models.go
+++ demux/database/models.go
@@ -16,6 +16,7 @@ type User struct {
 type Question struct {
 	gorm.Model
 	number             int
+	difficulty         int
 	round              int
 	reference_img      []byte
 	accuracy_threshold float64
